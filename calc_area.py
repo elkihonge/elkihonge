0e6ad05e1cee2aa4d65c71d226b07b1c18eb570b
unknown <kingkihonge@gmail.com>
2023-06-26 09:12:51 +0300

    area calculator

---
+++ calc_area.py
@@ -0,0 +1,11 @@
+#enable user to enter input
+width = input("Enter your width")
+length = input("Enter your length")
+
+#convert variables to integers
+Width  = int(width)
+Length = int(length)
+
+#perform calculation
+calc_area = (Length * Width)
+print(calc_area)
