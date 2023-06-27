9c409dda81333d7b714a0012063d0b6ab10671b6
unknown <kingkihonge@gmail.com>
2023-06-27 09:06:06 +0300

    scoresystem

---
+++ conditions.py
@@ -0,0 +1,7 @@
+a = 100
+b = 50
+
+if a > b:
+    print("True")
+else:
+    print("False")
---
+++ scoresystem.py
@@ -0,0 +1,16 @@
+marks = int(input("Enter your Marks: "))
+
+if marks < 40:
+    print(" E ")
+elif marks < 50:
+    print(" D ")
+elif marks < 60:
+    print(" C ")
+elif marks < 70:
+    print(" B ")
+
+else:
+    print(" A ")
+
+
+
