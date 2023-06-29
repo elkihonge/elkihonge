bbf33c83c1313971f84dd698f07989a657524dfd
unknown <kingkihonge@gmail.com>
2023-06-29 09:54:25 +0300

    Lists

---
+++ data structures/lists.py
@@ -0,0 +1,11 @@
+#defined using square brackets
+#You can modify dat - add, remove or update data in a list
+#A list has order
+
+friends = ["Bob", "Marie", "Sue"]
+friends.append("Roy")
+friends.append("Gill")
+friends.remove("Bob")
+
+print(friends)
+
