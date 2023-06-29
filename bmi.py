1c2db2605fb0ec2e7db4d14abbb10f0966bae66d
unknown <kingkihonge@gmail.com>
2023-06-29 08:41:55 +0300

    BMI scale added

--- bmi.py
+++ bmi.py
@@ -1,6 +1,6 @@
 #allow user to place input
 Weight = input("Enter weight")
-Height = input("Entere height")
+Height = input("Enter height")
 
 #put integers
 weight = float(Weight)
@@ -9,3 +9,13 @@ height = float(Height)
 #calculation
 bmi_calc = (weight/height**2)
 print(bmi_calc)
+
+#give it a scale
+if bmi_calc <= 18.0:
+    print("You are underweight")
+elif bmi_calc <= 29.0:
+    print("You are normal weight")
+elif bmi_calc <= 34.0:
+    print("You are overweight")
+else:
+    print("You are obese")
