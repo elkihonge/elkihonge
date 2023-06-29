bc30e591c6031f73af4a1c1140de243d6544901a
unknown <kingkihonge@gmail.com>
2023-06-29 09:23:38 +0300

    weight class

--- bmi.py
+++ bmi.py
@@ -1,6 +1,6 @@
 #allow user to place input
-Weight = input("Enter weight")
-Height = input("Enter height")
+Weight = input("Enter weight: ")
+Height = input("Enter height: ")
 
 #put integers
 weight = float(Weight)
@@ -12,10 +12,10 @@ print(bmi_calc)
 
 #give it a scale
 if bmi_calc <= 18.0:
-    print("You are underweight")
+    print("underweight")
 elif bmi_calc <= 29.0:
-    print("You are normal weight")
+    print("normal weight")
 elif bmi_calc <= 34.0:
-    print("You are overweight")
+    print("overweight")
 else:
-    print("You are obese")
+    print("obese")
