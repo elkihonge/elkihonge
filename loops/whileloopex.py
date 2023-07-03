3490542fe7cc5f8833c7c7d0c43c9a7f6323020f
unknown <kingkihonge@gmail.com>
2023-07-03 09:06:39 +0300

    whileloop exercise

---
+++ loops/whileloopex.py
@@ -0,0 +1,7 @@
+# using range 1 - 1000
+# print all numbers using while loop
+# ensure user inputs number in the terminal
+x = int(input("Enter number: "))
+while x < 1000:
+      print(x)
+      x += 1
