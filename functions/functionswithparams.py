0a14c816264c2b1f018221f30a189c361375eae2
unknown <kingkihonge@gmail.com>
2023-07-03 10:25:51 +0300

    function with parameter

---
+++ functions/functionswithparams.py
@@ -0,0 +1,10 @@
+def addnums(x, y):
+    result = x + y
+    print(result)
+addnums(100, 20)
+
+def showname(first, last):
+    print(first, last)
+
+showname(input("enter first name: "),input("enter last name: "))
+
