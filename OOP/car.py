15241ad88182109ae09d8d095e7f2f586ac6e9d4
unknown <kingkihonge@gmail.com>
2023-07-04 09:38:50 +0300

    OOP1

---
+++ OOP/car.py
@@ -0,0 +1,7 @@
+# blueprint / class
+class Car:
+    def __init__(self, make, model, owner, color):
+        self.make = make
+        self.model = model
+        self.owner = owner
+        self.color = color
---
+++ OOP/car_object.py
@@ -0,0 +1,11 @@
+from car import Car
+
+owner1 = Car("Toyota", "Vitz", "Kim", "Black")
+owner2 = Car("Toyota", "Vitz", "Abel", "Red")
+owner3 = Car("Toyota", "Vitz", "Josh", "Blue")
+owner4 = Car("Toyota", "Vitz", "Kibe", "Yellow")
+
+print(owner1.make, owner1.model, owner1.owner, owner1.color)
+print(owner2.make, owner2.model, owner2.owner, owner2.color)
+print(owner3.make, owner3.model, owner3.owner, owner3.color)
+print(owner4.make, owner4.model, owner4.owner, owner4.color)
