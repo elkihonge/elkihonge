2e138cc7283dd77b5a7bd1f7c7df77261523fb26
unknown <kingkihonge@gmail.com>
2023-07-04 09:02:27 +0300

    Module import

---
+++ Modules/module1.py
@@ -0,0 +1,10 @@
+def addtwonumbers(num1, num2):
+    return num1 + num2
+
+
+def multtwonums(num1, num2):
+    return num1 * num2
+
+
+def subtwonumbers(num1, num2):
+    return num1 - num2
---
+++ Modules/module2.py
@@ -0,0 +1,9 @@
+from module1 import *
+
+print(addtwonumbers(31,10))
+print(multtwonums(20,10))
+print(subtwonumbers(34,14))
+
+
+
+
