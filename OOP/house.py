c50426a76b55daa6d42b56cbbc4a0e9c15e2e8a1
unknown <kingkihonge@gmail.com>
2023-07-04 10:06:58 +0300

    OOPex

---
+++ OOP/house.py
@@ -0,0 +1,6 @@
+class House:
+    def __init__(self, location, paint, floor, door):
+        self.location = location
+        self.paint = paint
+        self.floor = floor
+        self.door = door
---
+++ OOP/house_object.py
@@ -0,0 +1,11 @@
+from house import House
+
+owner1 = House("Nairobi", "Yellow", "Tile", "Wooden")
+owner2 = House("Naivasha", "Black", "Stone", "Metallic")
+owner3 = House("Lavington", "Blue", "Wood", "Glass")
+owner4 = House("Rungai", "Pink", "Tile", "Lead")
+
+print(owner1.location, owner1.paint, owner1.floor, owner1.door)
+print(owner2.location, owner2.paint, owner2.floor, owner2.door)
+print(owner3.location, owner3.paint, owner3.floor, owner3.door)
+print(owner4.location, owner4.paint, owner4.floor, owner4.door)
